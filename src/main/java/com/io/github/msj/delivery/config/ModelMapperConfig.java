ae8c0efff3b7dbf585bb38834903ed0a2e61d1f7
Mateus-msj3 <mateussouzaads@outlook.com>
2024-01-17 19:59:50 -0300

    refactor: Remoção de comentários

--- src/main/java/com/io/github/msj/delivery/config/ModelMapperConfig.java
+++ src/main/java/com/io/github/msj/delivery/config/ModelMapperConfig.java
@@ -8,13 +8,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ModelMapperConfig {
 
-//    @Bean
-//    public ModelMapper modelMapper() {
-//        ModelMapper modelMapper = new ModelMapper();
-//        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-//        return modelMapper;
-//    }
-
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
